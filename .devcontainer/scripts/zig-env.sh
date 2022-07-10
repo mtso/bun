f89f47b776f9b59e1b176335e2d839c98e0abcf7
Matthew <matthewt@wepay.com>
2022-07-09 18:28:50 -0700

    Pin zls to 4e6564d7daec95b4fb51cadfe25973a87cac181a

--- .devcontainer/scripts/zig-env.sh
+++ .devcontainer/scripts/zig-env.sh
@@ -3,5 +3,6 @@
 curl -L https://github.com/Jarred-Sumner/vscode-zig/releases/download/fork-v1/zig-0.2.5.vsix >/home/ubuntu/vscode-zig.vsix
 git clone https://github.com/zigtools/zls /home/ubuntu/zls
 cd /home/ubuntu/zls
+git checkout 4e6564d7daec95b4fb51cadfe25973a87cac181a
 git submodule update --init --recursive --progress --depth=1
 zig build -Drelease-fast
